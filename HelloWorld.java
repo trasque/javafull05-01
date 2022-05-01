4dd8da9eaa035c2e2c3625c92daefbfdec02de27
trasque <taisajazz@gmail.com>
2022-05-01 22:15:01 +0900

    Add For and IF.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,20 @@
 public class HelloWorld {
     public static void main(String args[]) {
-        System.out.println("Hello, world.");
+        //10回くりかえして出力、その後 Hello! を出力
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello, world.");
+        }
+        System.out.println("Hello!");
+
+        System.out.println("========");
+
+        //同様の処理でIFも使用
+        for (int i = 0; i < 11; i++) {
+            if (i == 10) {
+                System.out.println("Hello!");
+            } else {
+                System.out.println("Hello, world.");
+            }
+        }
     }
 }
